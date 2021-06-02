4600a925c44c617625da78cc3eca50c656ef82c5
Bnanyingi <58061814+Bnanyingi@users.noreply.github.com>
2021-06-02 15:37:07 +0300

    backupdate

--- Ablog/settings.py
+++ Ablog/settings.py
@@ -119,6 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES = Path(BASE_DIR, 'static')
-VENV_PATH = Path(BASE_DIR)
-STATIC_ROOT = Path(BASE_DIR, 'static_root')
